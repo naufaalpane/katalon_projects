ffde3753837f3834b466235ad4a52d92aa121bad
Naufal Pane <naufal.pane@gmail.com>
2021-05-24 11:41:12 +0700

    created branch, added files

---
+++ Test Suites/Manual_TS.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Manual_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0f334983-8d8a-49e0-b6d2-f8478f9570f7</testSuiteGuid>
+   <testCaseLink>
+      <guid>ac1ad686-ab2f-46c6-af48-5bc771b5c902</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Record</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7580ed68-09c1-4690-a301-1ef0236c9405</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manual/TC_Script</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
